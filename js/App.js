1ab4d5d49d130f0db144acb497702d24c65552ed
kristofkania33 <zyhkana25@wp.pl>
2017-06-26 20:22:09 +0200

    better kanban

---
+++ js/App.js
@@ -0,0 +1,35 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '1933',
+  'X-Auth-Token': 'd10a0f1b41af991c54495f4c0bf59819'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+   url: baseUrl + '/board' ,
+   method: 'GET',
+   success: function(response) {
+   	setupColumns(response.columns);
+   }
+});
+
+function setupColumns(columns) {
+	columns.forEach(function(column) {
+		var col = new Column(column.id, column.name);
+	  board.createColumn(col);
+	  setupCards(col, column.cards);
+	});
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function(card){
+		var card = new Card(card.id,card.name,card.bootcamp_kanban_column_id);
+		col.createCard(card);
+	})
+}
+
+
+
